5c4b0e8ac32da87b05562af0f53562fc8838fb88
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-02 03:53:45 -0400

    modified the shell script.

--- linux_info.sh
+++ linux_info.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/bash
 
-echo "The kernel for this Operating System is" 
+echo "This is the summary information for the Linux Ubuntu distro version 22.04 LTS:"
+
+echo "1. The kernel for this Operating System is" 
 uname -s
 
-echo "it's version release is"
+echo "2. it's version release is"
 uname -r
 
-echo "it's version and not the release is"
+echo "3. it's version and not the release is"
 uname -v
 
-echo "The hostname for this Operating System is"
+echo "4. The hostname for this Operating System is"
 uname -n
 
-echo "The operating system is"
+echo "5. The operating system is"
 uname -o
