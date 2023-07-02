625e8639baf8f1b5b722b0dd03ed7d55073b22cb
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-02 03:42:11 -0400

    first commit for the program.

---
+++ linux_info.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+echo "The kernel for this Operating System is" 
+uname -s
+
+echo "it's version release is"
+uname -r
+
+echo "it's version and not the release is"
+uname -v
+
+echo "The hostname for this Operating System is"
+uname -n
+
+echo "The operating system is"
+uname -o
