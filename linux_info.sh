6e0d0106def80dae5573e7dacee961baacc61932
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-02 20:57:46 -0400

    incorporated data variables into the strings.

--- linux_info.sh
+++ linux_info.sh
@@ -2,17 +2,13 @@
 
 echo "This is the summary information for the Linux Ubuntu distro version 22.04 LTS:"
 
-echo "1. The kernel for this Operating System is" 
-uname -s
+echo "1. The kernel for this Operating System is `uname -s`" 
 
-echo "2. it's version release is"
-uname -r
+echo "2. it's version release is `uname -r`"
 
-echo "3. it's version and not the release is"
-uname -v
+echo "3. it's version and not the release is `uname -v`"
 
-echo "4. The hostname for this Operating System is"
-uname -n
+echo "4. The hostname for this Operating System is `uname -n`"
 
-echo "5. The operating system is"
-uname -o
+echo "5. The operating system is `uname -o`"
+
